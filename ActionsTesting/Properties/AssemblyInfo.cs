5da4b3f27be3cdce83a16958ad1bf99b9f15f368
GrouptreeHarvey <Harvey.Connolly@grouptree.com>
2022-08-09 16:35:15 +0100

    Commit

---
+++ ActionsTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ActionsTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ActionsTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c910442-7671-48f3-b369-9f2b7997ebbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ActionsTesting/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using ActionsTestProgram;
+
+namespace ActionsTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Person person = new Person("Harvey", 21);
+            Assert.AreEqual(21,person.age);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Person person = new Person("Harvey", 21);
+            person.haveBirthday();
+            Assert.AreEqual(22, person.age);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Person person = new Person("Harvey", 21);
+            Person person2 = new Person("Harry", 20);
+            person2.haveBirthday();
+            Assert.AreEqual(person.age, person2.age);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Person person = new Person("Harvey", Int32.MaxValue);
+            person.haveBirthday();
+            Assert.AreEqual(Int32.MinValue, person.age);
+        }
+
+    }
+}
